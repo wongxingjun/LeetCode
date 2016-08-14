0348448478716185ef5e060b4a51be4421a52bb2
Xingjun Wang <wongxingjun@126.com>
2016-08-14 19:12:21 +0800

    Update kthLargestElementInAnArray.cpp

--- kthLargestElementInAnArray.cpp
+++ kthLargestElementInAnArray.cpp
@@ -12,3 +12,15 @@ class Solution {
     Max heap
     Set a window of k ints
 */
+
+//max heap
+class Solution {
+    public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq(nums.begin(),nums.end());
+        for(int i=0;i<k-1;i++){
+            pq.pop();
+        }
+        return pq.top();
+    }
+}
