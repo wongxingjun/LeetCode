7731f6d39b6f76de384676109c9d460722c1de86
Xingjun Wang <wongxingjun@126.com>
2016-08-29 22:26:40 +0800

    Update findMinimumInRotatedSortedArray.cpp

--- findMinimumInRotatedSortedArray.cpp
+++ findMinimumInRotatedSortedArray.cpp
@@ -8,3 +8,21 @@ public:
         return min;
     }
 };
+
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int l=0,r=nums.size()-1;
+        while(l<r){
+            if(nums[l]<nums[r])
+                return nums[l];
+            int m=l+(r-l)/2;
+            if(nums[m]>=nums[l])
+                l=m+1;
+            else
+                r=m;
+        }
+        return nums[l];
+    }
+};
